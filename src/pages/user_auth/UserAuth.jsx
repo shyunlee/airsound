34827b56920ec589f26c1b5068137da0059b95eb
SEAN LEE <seanlee@MacBook-Pro.local>
2021-09-14 14:30:34 -0500

    style login & signup

--- src/pages/user_auth/UserAuth.jsx
+++ src/pages/user_auth/UserAuth.jsx
@@ -9,6 +9,10 @@ const UserAuth = (props) => {
   const [confirmPassword, setConfirmPassword] = useState('')
 
   const toggle = () => {
+    setEmail('')
+    setUsername('')
+    setPassword('')
+    setConfirmPassword('')
     if (useState === null) {
       setToggleLogin(false)
     } else {
@@ -40,11 +44,11 @@ const UserAuth = (props) => {
           <form className={styles.signup_input}>
             <label className={styles.label}>
               Email<br/>
-              <input className={styles.input} name='email' type="email" value={email} onChange={onChange}/>
+              <input className={styles.input} name='email' type="email" value={email} onChange={onChange} autoComplete='off'/>
             </label>
             <label className={styles.label}>
               User Name<br/>
-              <input className={styles.input} name='username' type="text" value={username} onChange={onChange}/>
+              <input className={styles.input} name='username' type="text" value={username} onChange={onChange} autoComplete='off'/>
             </label>
             <label className={styles.label}>
               Password<br/>
@@ -54,19 +58,25 @@ const UserAuth = (props) => {
               Confirm Password<br/>
               <input className={styles.input} name='confirmPassword' type="password" value={confirmPassword} onChange={onChange}/>
             </label>
+            <button className={styles.submit_button} type='submit'>Signup</button>
           </form>
+          <div className={styles.auth_container}>
+            <div className={styles.login_text}>Already have an account?</div>
+            <div className={styles.login_link} onClick={toggle}>Bck to login</div>
+            <AuthLogin />
+          </div>
         </div>
         <div className={styles.login_container}>
           <form className={styles.login_input}>
             <label className={styles.label}>
               Email<br/>
-              <input className={styles.input} name='email' type="email" value={email} onChange={onChange}/>
+              <input className={styles.input} name='email' type="email" value={email} onChange={onChange} autoComplete='off'/>
             </label>
             <label className={styles.label}>
               Password<br/>
               <input className={styles.input} name='password' type="password" value={password} onChange={onChange}/>
             </label>
-            <button className={styles.submit_button} type='submit'>{toggleLogin===null || toggleLogin===true? 'Login' : 'Signup'}</button>
+            <button className={styles.submit_button} type='submit'>Login</button>
           </form>
           <div className={styles.auth_container}>
             <div className={styles.login_text}>Don't have an account yet?</div>
@@ -84,7 +94,7 @@ const AuthLogin = () => {
   return (
     <div className={styles.auth_login}>
       <div className={styles.auth_divider}>
-        <p className={styles.text_or}>Or</p>
+        <p className={styles.text_or}>or</p>
       </div>
       <div className={styles.auth_btn_container}>
         <button className={styles.auth_btn}>Google</button>
